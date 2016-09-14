8074d5915eee4e605b61d221eab8ebf6e6c742bd
Anderson Tavares <nocturne.pe@gmail.com>
2016-09-14 02:08:13 -0300

    Estruturando Node
    - Criando o pacote
    - Instalando dependências
    - Criando config do TypeScript
    - Criando config do Typings
    - Criando um servidor
    - Removendo arquivos JavaScript

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import * as koa from 'koa';
+import * as mongoose from 'mongoose';
+var app:koa     = new koa();
+
+// Connecting to our database (MongoLab from Heroku) or just local
+var uristringDev:string  = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/ajudati_dev';
+var uristringProd:string = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/ajudati_prod';
+var uristringTest:string = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/ajudati_test';
+mongoose.connect(uristringDev);
+var port:number = process.env.PORT || 8000;
+
+app.use(function *(){
+  this.body = 'Bem Vindo ao AjudaTI';
+});
+
+app.listen(port);
+console.log('AjudaTI executando');
