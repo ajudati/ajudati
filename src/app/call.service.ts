7c8e35c40a3ae377b9753cae597ed4ab7c1d1adb
Anderson Tavares <nocturne.pe@gmail.com>
2016-09-26 18:36:51 -0300

    Inserindo chamado no banco
    - Ao preencher o formulário do chamado, o botão "Procurar ajudantes" fica habilitado
    - Ao clicar em "Procurar ajudantes", o formulário envia ao Firebase e guarda o chamado no serviço (currentCall)
    - A tela de busca de ajudantes procurará o chamado no serviço de chamados (currentCall)

--- src/app/call.service.ts
+++ src/app/call.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import { Call } from './call';
+
 @Injectable()
 export class CallService {
 
-  constructor(private af:AngularFire) { }
+  currentCall:Call;
+
+  constructor(private af:AngularFire) {
+    this.currentCall = null;
+  }
+
+  createCall(call:Call): firebase.Promise<any>{
+    return this.af.database.list(`calls`).push(call);
+  }
+
 
   getCalls(uid:string):FirebaseListObservable<any>{
     return this.af.database.list('calls',{
